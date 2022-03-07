dbf2686870c2ae8faf20eaf90a4f0cbf0520be9d
KBR <kbr@ucl.dk>
2022-03-07 18:37:10 +0100

    Unit test refactoring

--- Booking.Domain.Tests/BookingOverlapTests.cs
+++ Booking.Domain.Tests/BookingOverlapTests.cs
@@ -7,12 +7,12 @@ using Xunit;
 
 namespace Booking.Domain.Tests;
 
-public class BookingTests
+public class BookingOverlapTests
 {
     private readonly IServiceProvider _serviceProvider;
     private readonly List<Entities.Booking> _exsistingBookings;
 
-    public BookingTests()
+    public BookingOverlapTests()
     {
         _exsistingBookings = new List<Entities.Booking>(new[]
         {
@@ -23,71 +23,8 @@ public class BookingTests
         bookingDomainServiceMock.Setup(foo => foo.GetExsistingBookings()).Returns(_exsistingBookings);
 
         var serviceCollection = new ServiceCollection();
-        serviceCollection.AddScoped(sp => bookingDomainServiceMock.Object);
+        serviceCollection.AddScoped(_ => bookingDomainServiceMock.Object);
         _serviceProvider = serviceCollection.BuildServiceProvider();
-    }
-
-    [Fact]
-    public void Given_Start_Is_Default_Value__Then_ArgumentOutOfRangeException_Is_Thrown()
-    {
-        // Arrange
-        var slut = DateTime.Parse("2022-1-1 09:00");
-        var expected = "Start dato skal være udfyldt (Parameter 'start')";
-
-        // Act
-        Action action = () => new Entities.Booking(_serviceProvider, default, slut);
-
-        //Assert
-        var caughtException = Assert.Throws<ArgumentOutOfRangeException>(action);
-        Assert.Equal(expected, caughtException.Message);
-    }
-
-    [Fact]
-    public void Given_Slut_Is_Default_Value__Then_ArgumentOutOfRangeException_Is_Thrown()
-    {
-        // Arrange
-        var start = DateTime.Parse("2022-1-1 08:00");
-        var expected = "Slut dato skal være udfyldt (Parameter 'slut')";
-
-        // Act
-        Action action = () => new Entities.Booking(_serviceProvider, start, default);
-
-        //Assert
-        var caughtException = Assert.Throws<ArgumentOutOfRangeException>(action);
-        Assert.Equal(expected, caughtException.Message);
-    }
-
-    [Theory]
-    [InlineData("2022-1-1 08:00", "2022-1-1 07:59")]
-    [InlineData("2022-1-1 08:00", "2022-1-1 08:00")]
-    public void Given_Slut_Is_Not_After_Start__Then_Exception_Is_Thrown(string startStr, string slutStr)
-    {
-        // Arrange
-        var start = DateTime.Parse(startStr);
-        var slut = DateTime.Parse(slutStr);
-        var expected = $"Slut dato/tid skal være senere end start (start, slut): {start}, {slut}";
-
-        // Act
-        Action action = () => new Entities.Booking(_serviceProvider, start, slut);
-
-        //Assert
-        var caughtException = Assert.Throws<Exception>(action);
-        Assert.Equal(expected, caughtException.Message);
-    }
-
-    [Theory]
-    [InlineData("2022-1-1 08:00", "2022-1-1 09:00")]
-    public void Given_No_Overlap__Then_Booking_Is_Created(string startStr, string slutStr)
-    {
-        // Arrange
-        var start = DateTime.Parse(startStr);
-        var slut = DateTime.Parse(slutStr);
-
-        // Act
-        var sut = new Entities.Booking(_serviceProvider, start, slut);
-
-        //Assert
-        Assert.NotNull(sut);
     }
 
 
@@ -99,19 +36,19 @@ public class BookingTests
     [InlineData("2022-1-1 11:00", "2022-1-1 12:00", "Start berører eksisterende booking")]
     [InlineData("2022-1-1 10:30", "2022-1-1 10:45", "Ny boking ligger inde i eksisterende booking")]
     [InlineData("2022-1-1 09:00", "2022-1-1 12:00", "Eksisterende booking ligger inde i ny booking")]
-    public void Given_Overlap__Then_Exception_Is_Thrown(string startStr, string slutStr, string userMessage)
+    public void Given_Overlap__Then_IsOverlapping_Is_True(string startStr, string slutStr, string userMessage)
     {
         // Arrange
         var start = DateTime.Parse(startStr);
         var slut = DateTime.Parse(slutStr);
-        var expected = "Booking overlapper med eksisterende booking";
+        var sut = new BookingStub(new Guid(), start, slut);
+        sut.ServiceProvider = _serviceProvider;
 
         // Act
-        Action action = () => new Entities.Booking(_serviceProvider, start, slut);
+        var actual = sut.IsOverlapping();
 
         //Assert
-        var caughtException = Assert.Throws<Exception>(action);
-        Assert.True(expected.Equals(caughtException.Message), userMessage);
+        Assert.True(actual, userMessage);
     }
 
 
@@ -149,5 +86,21 @@ public class BookingTests
         //Assert
         var caughtException = Assert.Throws<Exception>(action);
         Assert.True(expected.Equals(caughtException.Message), userMessage);
+    }
+
+    public class BookingStub : Entities.Booking
+    {
+        public BookingStub(Guid id, DateTime start, DateTime slut) : base(id, start, slut)
+        {
+        }
+
+        public BookingStub(IServiceProvider serviceProvider, DateTime start, DateTime slut) : base(serviceProvider, start, slut)
+        {
+        }
+
+        public new bool IsOverlapping()
+        {
+            return base.IsOverlapping();
+        }
     }
 }
--- Booking.Domain/Entities/Booking.cs
+++ Booking.Domain/Entities/Booking.cs
@@ -5,8 +5,6 @@ namespace Booking.Domain.Entities;
 
 public class Booking
 {
-    public IServiceProvider? ServiceProvider { get; set; }
-
     // DO NOT USE - EF and TEST ONLY!!!
     public Booking(Guid id, DateTime start, DateTime slut)
     {
@@ -17,7 +15,7 @@ public class Booking
 
     public Booking(IServiceProvider serviceProvider, DateTime start, DateTime slut)
     {
-        if (start == default) throw new ArgumentOutOfRangeException(nameof(start),"Start dato skal være udfyldt");
+        if (start == default) throw new ArgumentOutOfRangeException(nameof(start), "Start dato skal være udfyldt");
         if (slut == default) throw new ArgumentOutOfRangeException(nameof(slut), "Slut dato skal være udfyldt");
         if (start >= slut)
             throw new Exception($"Slut dato/tid skal være senere end start (start, slut): {start}, {slut}");
@@ -29,22 +27,31 @@ public class Booking
         Id = Guid.NewGuid();
     }
 
-    public Guid Id { get; private set; }
+    public IServiceProvider? ServiceProvider { get; set; }
+
+    public Guid Id { get; }
     public DateTime Start { get; private set; }
     public DateTime Slut { get; private set; }
 
-    protected bool IsOverlapping()
+    protected virtual bool IsOverlapping()
     {
         var bookingDomainService = ServiceProvider?.GetService<IBookingDomainService>();
         if (bookingDomainService == null) throw new Exception("Implementation of IBookingDomainService was not found");
 
         return bookingDomainService.GetExsistingBookings()
             .Any(a => a.Start >= Start && a.Start <= Slut || a.Slut >= Start && a.Slut <= Slut);
-        //.Any(a => a.Id != Id && (a.Start >= Start && a.Start <= Slut || a.Slut >= Start && a.Slut <= Slut));
+        // https://stackoverflow.com/questions/325933/determine-whether-two-date-ranges-overlap
+        //.Any(a => a.Start <= Slut && Start <= a.Slut);
+        //.Any(a => a.Id != Id && a.Start <= Slut && Start <= a.Slut);
     }
 
     public void Update(DateTime start, DateTime slut)
     {
+        if (start == default) throw new ArgumentOutOfRangeException(nameof(start), "Start dato skal være udfyldt");
+        if (slut == default) throw new ArgumentOutOfRangeException(nameof(slut), "Slut dato skal være udfyldt");
+        if (start >= slut)
+            throw new Exception($"Slut dato/tid skal være senere end start (start, slut): {start}, {slut}");
+
         Start = start;
         Slut = slut;
         if (IsOverlapping()) throw new Exception("Booking overlapper med eksisterende booking");
