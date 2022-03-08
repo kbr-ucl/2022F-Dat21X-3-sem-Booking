532419199c6764552fa27176ca66f4f93decb1ad
KBR <kbr@ucl.dk>
2022-03-08 08:42:11 +0100

    Tomme "Delete" og "Details" pages er oprettet

---
+++ Booking.Web/Pages/Booking/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Booking.Web.Pages.Booking
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
