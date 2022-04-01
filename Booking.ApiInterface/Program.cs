3ab3e12f52e358d016aa78718727fb85e38d250a
KBR <kbr@ucl.dk>
2022-04-01 13:15:31 +0200

    IoC Scrutor added to Booking.Api

--- Booking.ApiInterface/Program.cs
+++ Booking.ApiInterface/Program.cs
@@ -1,11 +1,7 @@
 using System.Reflection;
-using Booking.Application.Contract;
 using Booking.Application.Implementation;
-using Booking.Application.Infrastructure;
-using Booking.Domain.DomainServices;
 using Booking.Infrastructure.Database;
 using Booking.Infrastructure.DomainServicesImpl;
-using Booking.Infrastructure.Queries;
 using Booking.Infrastructure.RepositoriesImpl;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
@@ -18,17 +14,22 @@ var builder = WebApplication.CreateBuilder(args);
 // Update-Database
 
 builder.Services.AddDbContext<BookingContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("BookingContext"), x =>
-    {
-        x.MigrationsAssembly("Booking.Infrastructure");
-    }));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BookingContext"),
+        x => { x.MigrationsAssembly("Booking.Infrastructure"); }));
 
-builder.Services.AddScoped<IBookingQuery, BookingQuery>();
-builder.Services.AddScoped<IBookingCommand, BookingCommand>();
-builder.Services.AddScoped<IBookingRepository, BookingRepository>();
-builder.Services.AddScoped<IBookingRepository, BookingRepository>();
-builder.Services.AddScoped<IBookingDomainService, BookingDomainService>();
 
+builder.Services.Scan(scan =>
+{
+    scan.FromCallingAssembly()
+        .AddClasses()
+        .AsMatchingInterface();
+    scan.FromAssembliesOf(typeof(BookingCommand)).AddClasses()
+        .AsMatchingInterface();
+    scan.FromAssembliesOf(typeof(BookingDomainService)).AddClasses()
+        .AsMatchingInterface();
+    scan.FromAssembliesOf(typeof(BookingRepository)).AddClasses()
+        .AsMatchingInterface();
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
